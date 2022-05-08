880ee3593c741be95bfa9fd3df58a143d85778be
Emiliobau <emiliobau@hotmail.com>
2022-05-08 10:40:56 -0300

    fixesII for heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,14 +14,14 @@ module.exports = {
   "test": {
     "username": "root",
     "password": null,
-    "database": "database_test",
+    "database": "cambalache",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": null,
-    "database": "database_production",
+    "database": "cambalache",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
